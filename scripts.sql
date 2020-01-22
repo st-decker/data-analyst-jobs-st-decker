74f208a7eed81511e9eae70d002c697a0842467c
st-decker <37890176+st-decker@users.noreply.github.com>
2020-01-21 19:06:33 -0600

    All answers to HW 1

--- scripts.sql
+++ scripts.sql
@@ -48,13 +48,21 @@ WHERE location = 'CA';*/
 --Q9 double check, think of using HAVING
 --70
 /*SELECT COUNT(company)
-FROM (SELECT company, AVG(star_rating) AS avg_rate
+SELECT company, AVG(star_rating) AS avg_rate
 	FROM data_analyst_jobs
   	WHERE company IS NOT NULL
   	GROUP BY company  
   	HAVING SUM (review_count) > 5000) as test;*/
 	
 --Q10
+--correct answer below
+/*SELECT company, AVG(star_rating) AS avg_rate
+	FROM data_analyst_jobs
+  	WHERE company IS NOT NULL
+  	GROUP BY company  
+  	HAVING SUM(review_count) > 5000
+	ORDER BY avg_rate DESC;*/
+	
 --Mircosoft, 4.2 rounded
 /*SELECT company, AVG(star_rating) AS avg_rate
 FROM data_analyst_jobs
@@ -63,14 +71,15 @@ GROUP BY company, review_count
 ORDER BY avg_rate DESC;*/
 
 --Q11
---1669
-/*SELECT COUNT(title)
+--774
+/*SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE title ILIKE '%Analyst%'*/
 
 --Q12
 --4
 --Tableau
-/*SELECT title
+/*
+SELECT title
 FROM data_analyst_jobs
 WHERE title NOT ILIKE '%Anal%';*/
